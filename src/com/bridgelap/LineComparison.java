67c7ab7ccb1cb2d248634d548a75e83c34f3a23c
HP <sachdevamohita5@gmail.com>
2022-07-14 14:39:59 +0530

    welcome msg

---
+++ src/com/bridgelap/LineComparison.java
@@ -0,0 +1,8 @@
+package com.bridgelap;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println(" Welcome  to the Line Comparison Computation Program");
+    }
+
+}
