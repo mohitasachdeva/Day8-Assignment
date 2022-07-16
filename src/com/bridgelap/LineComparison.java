52e716977020bf4b9da1914f245233299772786b
HP <sachdevamohita5@gmail.com>
2022-07-16 15:17:05 +0530

    length1 of the line

--- src/com/bridgelap/LineComparison.java
+++ src/com/bridgelap/LineComparison.java
@@ -14,5 +14,18 @@ public class LineComparison {
             y2 = sc.nextFloat();
             double length = Math.sqrt( Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
             System.out.println(length);
+            System.out.println("Enter x1 y1 x2 y2 of line two: ");
+            x1 = sc.nextFloat();
+            y1 = sc.nextFloat();
+            x2 = sc.nextFloat();
+            y2 = sc.nextFloat();
+
+            double length1 = Math.sqrt( Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
+            System.out.println(length1);
+
+            if (length==length1){
+                System.out.println("Both the lines are equal in length. ");
+            }else
+                System.out.println("Both the lines are not equal in length. ");
         }
-    }
+}
